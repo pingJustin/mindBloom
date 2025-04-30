4af50c5e99fc7c1754eca1b7541f2160711da41a
pebbsmom <perezteri@hotmail.com>
2025-04-29 20:07:38 -0400

    added middleware

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -7,17 +7,17 @@ const router = express.Router();
 
 // Signup
 router.post('/signup', async (req, res) => {
-  const { username, email, password } = req.body;
+  const { email, password } = req.body;
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) return res.status(400).json({ message: 'User already exists' });
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ username, email, password: hashedPassword });
+    const newUser = await User.create({email, password: hashedPassword });
 
     const token = signToken(newUser.email, newUser._id);
 
-    res.status(201).json({ token, user: { id: newUser._id, username: newUser.username } });
+    res.status(201).json({ token, user: { id: newUser._id} });
   } catch (error) {
     console.error('Signup Error:', error);
     res.status(500).json({ message: 'Server error during signup' });
@@ -36,7 +36,7 @@ router.post('/login', async (req, res) => {
 
     const token = signToken(user.email, user._id);
 
-    res.status(200).json({ token, user: { id: user._id, username: user.username } });
+    res.status(200).json({ token, user: { id: user._id} });
   } catch (error) {
     console.error('Login Error:', error);
     res.status(500).json({ message: 'Server error during login' });
